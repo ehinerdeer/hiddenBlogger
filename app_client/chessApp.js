bb01f0de0fa75cf6efce7c0fca73778fc74db17b
Eric W Hinerdeer <ewhinerd@lampson.muad.local>
2019-05-01 11:58:55 -0400

    test dom stuff

--- app_client/chessApp.js
+++ app_client/chessApp.js
@@ -91,20 +91,15 @@ app.controller('ChessCtrl' , ['$http', '$scope', '$document', function ChessCtrl
   var vm = this;
   vm.title = "Chess";
   vm.message = "Don't Lose The Queen!";
- // vm.test1 = angular.element(document.getElementById("A1")); //gets element by id
+  vm.test1 = angular.element(document.getElementById("A1")); //gets element by id
 
-  //vm.test2 = vm.test1.html(); // gets innerhtml
-  vm.test1 = "";
-  vm.test2 = "";
-  vm.test3 = ""; 
+  vm.test2 = ""; // gets innerhtml
+  
   vm.clicks = 0;
   var space = "";
 
-  $scope.click = function(square) {
-  	space = angular.element(document.getElementById(square));
-  	var temp = space.html();
-  	vm.test1 = "";
-  	vm.test2 = temp;
+  $scope.click = function() {
+  	vm.test2 = vm.test1.html();
   }
     
 }]);
